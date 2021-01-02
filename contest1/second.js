38d960f2d43f6d793755b315faf11ead0d5efaff
natnaelawel <natnael.awel@gmail.com>
2021-01-02 17:01:44 +0300

    third question

--- contest1/second.js
+++ contest1/second.js
@@ -1,7 +1,7 @@
 function sumArray(n, k, ar) {
     let resultArray = []
     for (let i = 0; i < n - 1; i++) {
-        for (let j = 1; j <= n - 1; j++) {
+        for (let j = i + 1; j <= n - 1; j++) {
             let result = ar[i] + ar[j]
             if ((i < j) && result % k === 0) {
                 resultArray.push([i, j])
